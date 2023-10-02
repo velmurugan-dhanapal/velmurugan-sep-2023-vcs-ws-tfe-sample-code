feb09b8cff02976c611bdb7e9c3bdeeae5448fcd
velmurugan-dhanapal <54856501+velmurugan-dhanapal@users.noreply.github.com>
2023-10-01 20:43:54 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+
+
+/* cloud {
+    organization = "velmurugan-sep-2023"
+
+    workspaces {
+      name = "velmurugan-sep-2023-cli-ws"
+    }
+  } */
+}
